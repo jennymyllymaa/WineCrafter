121c426b5aa98ef3f0d68a722c3fcb09f03d4c3e
Noora Vainionpaa <noora.t.vainionpaa@tuni.fi>
2023-03-03 01:52:28 +0200

    Adding Menu and fruits for first game

---
+++ Assets/Code/Menu.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace WineCrafter
+{
+    public class Menu : MonoBehaviour
+    {
+        public void PlayGame()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+
+        public void QuitGame()
+        {
+            Debug.Log("QUIT!");
+            Application.Quit();
+        }
+    }
+}
