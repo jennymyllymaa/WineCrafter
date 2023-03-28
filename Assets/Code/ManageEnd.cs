95500cb1b04ed6ef03ec31ea91713ca4b2cfcbc4
Noora Vainionpaa <noora.t.vainionpaa@tuni.fi>
2023-03-28 19:34:57 +0300

    fixattu timescale bugi

--- Assets/Code/ManageEnd.cs
+++ Assets/Code/ManageEnd.cs
@@ -7,7 +7,8 @@ public class ManageEnd : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Time.timeScale = 1.0f;
+        Debug.Log("Moi");
+        //Time.timeScale = 1.0f;
     }
 
     // Update is called once per frame
