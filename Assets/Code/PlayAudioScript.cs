c096bba63139e8844608a53bade85a1ce0a1742b
jennymyllymaa <jennyinen@gmail.com>
2023-04-04 19:35:24 +0300

    peliäänet tuotu audio kansioon, taustaääni kaikissa paitsi score scenessä ja menun buttoneissa audiot

---
+++ Assets/Code/PlayAudioScript.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace WineCrafter
+{
+    public class PlayAudioScript : MonoBehaviour
+    {
+
+        Button CreditsButton;
+
+        private void Start()
+        {
+            CreditsButton = GetComponent<Button>();
+        }
+
+        public void PlayAudio()
+        {
+            AudioSource audio = CreditsButton.GetComponent<AudioSource>();
+
+            if (audio != null)
+            {
+                audio.Play();
+            }
+        }
+    }
+}
